43428c65c26dfb7553612247117c73449d65dc0e
Jakub <jakub.kolinger.024@pslib.cz>
2026-05-29 09:11:27 +0200

    add login component with styles and routing; refactor user to student components

---
+++ src/main/frontend/src/app/removeuser/removeuser.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-removeuser',
+  standalone: true,
+  imports: [],
+  templateUrl: './removeuser.component.html',
+  styleUrl: './removeuser.component.css'
+})
+export class RemoveuserComponent {
+
+}
