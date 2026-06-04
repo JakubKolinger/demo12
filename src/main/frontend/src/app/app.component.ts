d85f0352b183796fbc26f95f1212eaf467ccef3f
Jakub <jakub.kolinger.024@pslib.cz>
2026-06-04 11:48:36 +0200

    feat: enhance layout and responsiveness of app component with new styles and structure

--- src/main/frontend/src/app/app.component.ts
+++ src/main/frontend/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import {RouterModule, RouterOutlet} from '@angular/router';
+import { RouterModule, RouterOutlet, Router } from '@angular/router'; // Přidán import Router
+import { CommonModule } from '@angular/common'; // Přidán import CommonModule pro funkčnost *ngIf
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule ],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    CommonModule // Přidáno sem, aby fungoval *ngIf v app.component.html
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+
+  // Konstruktor, který zpřístupní aktuální URL adresu pro HTML šablonu
+  constructor(public router: Router) {}
 }
