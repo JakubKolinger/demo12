147ff6c0455c918d1be8e4820ba445386ca4591a
Jakub <jakub.kolinger.024@pslib.cz>
2026-06-04 13:01:11 +0200

    fix: add RouterLinkActive to imports in AppComponent for improved navigation functionality

--- src/main/frontend/src/app/app.component.ts
+++ src/main/frontend/src/app/app.component.ts
@@ -1,21 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet, Router } from '@angular/router'; // Přidán import Router
-import { CommonModule } from '@angular/common'; // Přidán import CommonModule pro funkčnost *ngIf
+import { RouterModule, RouterOutlet, Router, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    RouterModule,
-    CommonModule // Přidáno sem, aby fungoval *ngIf v app.component.html
-  ],
+  imports: [RouterOutlet, RouterModule, RouterLinkActive, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
 
-  // Konstruktor, který zpřístupní aktuální URL adresu pro HTML šablonu
+  // DŮLEŽITÉ: Musí zde být slůvko "public", aby šablona viděla metodu router.isActive()
   constructor(public router: Router) {}
 }
