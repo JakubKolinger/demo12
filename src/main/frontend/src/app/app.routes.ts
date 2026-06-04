c7f10ea0fb26825bcd829560611ec7d151521725
Jakub <jakub.kolinger.024@pslib.cz>
2026-06-04 13:21:35 +0200

    feat: remove login link from header and add Modifyuser route for user management

--- src/main/frontend/src/app/app.routes.ts
+++ src/main/frontend/src/app/app.routes.ts
@@ -2,17 +2,17 @@ import { Routes } from '@angular/router';
 import {UsersComponent} from './users/users/users.component';
 import {ContactsComponent} from './contacts/contacts.component';
 import {AboutComponent} from './about/about.component';
-import {LoginComponent} from './login/login.component';
 import {AdduserComponent} from './users/adduser/adduser.component';
 import {RemoveuserComponent} from './users/removeuser/removeuser.component';
+import {ModifyuserComponent} from './users/modifyuser/modifyuser.component';
 
 export const routes: Routes = [
 
   {path: 'users', component: UsersComponent},
   {path: 'contacts', component: ContactsComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'login', component: LoginComponent},
   {path: 'user/add', component: AdduserComponent},
   {path: 'user/remove', component: RemoveuserComponent},
+  {path: 'user/modify', component: ModifyuserComponent},
 
 ];
