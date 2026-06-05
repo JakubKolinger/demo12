51d759f81698f0bf823edee8c1db3e5d8f906156
Jakub <jakub.kolinger.024@pslib.cz>
2026-06-05 08:28:23 +0200

    feat: enhance user interface styles for add and remove user components

--- src/main/frontend/src/app/app.routes.ts
+++ src/main/frontend/src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AboutComponent } from './about/about.component';
+import { ModifyuserComponent } from "./users/users/modifyuser/modifyuser.component";
 import {UsersComponent} from './users/users/users.component';
-import {ContactsComponent} from './contacts/contacts.component';
-import {AboutComponent} from './about/about.component';
 import {AdduserComponent} from './users/users/adduser/adduser.component';
 import {RemoveuserComponent} from './users/users/removeuser/removeuser.component';
-import {ModifyuserComponent} from './users/users/modifyuser/modifyuser.component';
 
 export const routes: Routes = [
+  // 1. POKUD UŽIVATEL ZADÁ /home, PŘESMĚRUJ HO NA ČISTÉ /
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+
+  // 2. ČISTÁ TRASA PRO DOMOVSKOU STRÁNKU (Nespouští žádnou komponentu, obsah je v app.component)
+  { path: '', children: [] },
 
   {path: 'users', component: UsersComponent},
   {path: 'contacts', component: ContactsComponent},
